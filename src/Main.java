6b9e8c3fdebe7c948b38e991b2afa69973895679
micodian <micodian83@gmail.com>
2020-11-12 22:41:59 -0600

    added Queue data structure

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import DataStructures.Stacks;
 
 public class Main  {
@@ -7,28 +9,10 @@ public class Main  {
 
         
        
-        Stacks myStack = new Stacks(7);
-        // myStack.push(12);
-        // myStack.push(13);
-        // myStack.push(34);
-        // myStack.push(54);
-        // myStack.push(87);
-        // myStack.push(47);
-        // myStack.push(47);
-        // myStack.push(47);
-
+        
        
 
-        System.out.println(myStack.isFull());
-
-        // System.out.println(myStack.peek());
-        System.out.println(myStack.pop());
-
-        // while (!myStack.isEmpty()) {
-        //     long value = myStack.pop();
-        //     System.out.println(value);
-        // }    
-        
+       
     }
     
 }
