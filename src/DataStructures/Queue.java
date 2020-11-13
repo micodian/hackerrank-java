59773f2d90f8e81cb695ff0e8406855fc15575d2
micodian <micodian83@gmail.com>
2020-11-12 23:19:00 -0600

    implemented the queue data structure

--- src/DataStructures/Queue.java
+++ src/DataStructures/Queue.java
@@ -1,5 +1,64 @@
 package DataStructures;
 
 public class Queue {
-    
+    private int maxSize;
+    private int front;
+    private int rear;
+    private long[] queueArray;
+    private long count;
+
+    public Queue(int size){
+        this.maxSize = size;
+        this.queueArray = new long[maxSize];
+        front = 0;
+        rear = -1;
+        this.count = 0;
+
+
+    }
+
+    public void insert(long value){
+
+        if (isFull()) {
+            System.out.println("The Queue is full, Please remove an item to add another one");
+        } else {
+        rear++;
+        queueArray[rear]= value;
+        count++;
+        }
+        
+    }
+
+    public long remove(){
+        
+        long temp = queueArray[front];
+        front++;
+        count --;
+        return temp;
+    }
+
+    public long peek(){
+        return queueArray[front];
+    }
+
+    public long peekRear(){
+        return queueArray[rear];
+    }
+
+    public boolean isFull(){
+        return (count == maxSize);
+    }
+
+    public boolean isEmpty(){
+        return (count == 0);
+    }
+
+    public void printQueue(){
+        System.out.print("[ ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(queueArray[i] + " ");
+        }
+        System.out.print(" ]");
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 
+import DataStructures.Queue;
 import DataStructures.Stacks;
 
 public class Main  {
@@ -8,8 +9,17 @@ public class Main  {
     public static void main(String[] args) {
 
         
-       
-        
+       Queue myQueue = new Queue(5);
+       myQueue.insert(32);
+       myQueue.insert(35);
+       myQueue.insert(399);
+       myQueue.insert(35888);
+       myQueue.insert(35888);
+
+      
+      System.out.println(myQueue.isFull()); 
+      myQueue.insert(35888);
+
        
 
        
